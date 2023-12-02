215cd1aecc3ee847009116547e3e2f7904e9c5f2
Mike Hsu <mike.hsu@gmail.com>
2023-12-02 14:05:11 -0800

    test drag and dropping

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,28 @@
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::prelude::*;
+use bevy_mod_picking::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, DefaultPickingPlugins))
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("icon.png"),
-        ..Default::default()
-    });
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("icon.png"),
+            ..Default::default()
+        },
+        PickableBundle::default(),
+        On::<Pointer<DragStart>>::target_insert(Pickable::IGNORE), // Disable picking
+        On::<Pointer<DragEnd>>::target_insert(Pickable::default()), // Re-enable picking
+        On::<Pointer<Drag>>::target_component_mut::<Transform>(|drag, transform| {
+            transform.translation.x += drag.delta.x; // Make the square follow the mouse
+            transform.translation.y -= drag.delta.y;
+        }),
+    ));
 }
